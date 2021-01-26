14e1335d3a410881611669e268cd9062c4944173
viniciusanchieta <vinicius.anchieta@outlook.com>
2021-01-26 00:15:50 -0300

    Link of my user github added

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner />
+      <GitHubCorner projectUrl="https://github.com/viniciusanchieta" />
     </QuizBackground>
   );
 }
